d71e153dec7ad22d3506edb7c3bc26c6a39ab0ef
CINDY-NB\Cindy <ilovecoco309@gmail.com>
2017-08-30 11:17:05 +0800

    第一週

---
+++ script.js
@@ -0,0 +1,16 @@
+function showMsg(msg1+msg2){
+    var allMsg = msg1+msg2;
+    return allMsg;
+}
+
+function lastChar(str){
+    var index = 
+    str[str.length-1];
+    return(index);
+}
+
+function random(m){
+    var index = 
+    str[str.length-1];
+    return(index);
+}
